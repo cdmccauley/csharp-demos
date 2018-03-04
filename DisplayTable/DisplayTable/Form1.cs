bc32b1e8bdf98e61974795e3ea41a44dfd70f948
cdmccauley <cdmccauley@gmail.com>
2018-03-03 18:47:10 -0700

    DisplayTable screenshots aquired

---
+++ DisplayTable/DisplayTable/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplayTable
+{
+    public partial class DisplayAuthorsTable : Form
+    {
+        public DisplayAuthorsTable()
+        {
+            InitializeComponent();
+        }
+
+        private BooksDataContext database = new BooksDataContext();
+
+        private void DisplayAuthorsTable_Load(object sender, EventArgs e)
+        {
+            authorBindingSource.DataSource =
+                from author in database.Authors
+                orderby author.AuthorID
+                select author;
+        }
+
+        private void authorBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            Validate();
+            authorBindingSource.EndEdit();
+            database.SubmitChanges();
+        }
+    }
+}
