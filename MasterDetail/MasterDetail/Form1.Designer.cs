8994f62f8ce30af6ec95040a6dfc0f93c4c86953
cdmccauley <cdmccauley@gmail.com>
2018-03-04 15:31:54 -0700

    MasterDetail screenshots captured

---
+++ MasterDetail/MasterDetail/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace MasterDetail
+{
+    partial class Details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.authorLabel = new System.Windows.Forms.Label();
+            this.authorComboBox = new System.Windows.Forms.ComboBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.titleComboBox = new System.Windows.Forms.ComboBox();
+            this.booksDataGridView = new System.Windows.Forms.DataGridView();
+            this.titleBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.authorBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.booksDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titleBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.authorBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // authorLabel
+            // 
+            this.authorLabel.AutoSize = true;
+            this.authorLabel.Location = new System.Drawing.Point(12, 15);
+            this.authorLabel.Name = "authorLabel";
+            this.authorLabel.Size = new System.Drawing.Size(41, 13);
+            this.authorLabel.TabIndex = 0;
+            this.authorLabel.Text = "Author:";
+            // 
+            // authorComboBox
+            // 
+            this.authorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.authorComboBox.FormattingEnabled = true;
+            this.authorComboBox.Location = new System.Drawing.Point(59, 12);
+            this.authorComboBox.Name = "authorComboBox";
+            this.authorComboBox.Size = new System.Drawing.Size(177, 21);
+            this.authorComboBox.TabIndex = 1;
+            this.authorComboBox.SelectedIndexChanged += new System.EventHandler(this.authorComboBox_SelectedIndexChanged);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(242, 18);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(30, 13);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "Title:";
+            // 
+            // titleComboBox
+            // 
+            this.titleComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.titleComboBox.FormattingEnabled = true;
+            this.titleComboBox.Location = new System.Drawing.Point(278, 12);
+            this.titleComboBox.Name = "titleComboBox";
+            this.titleComboBox.Size = new System.Drawing.Size(194, 21);
+            this.titleComboBox.TabIndex = 3;
+            this.titleComboBox.SelectedIndexChanged += new System.EventHandler(this.titleComboBox_SelectedIndexChanged);
+            // 
+            // booksDataGridView
+            // 
+            this.booksDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.booksDataGridView.Location = new System.Drawing.Point(12, 42);
+            this.booksDataGridView.Name = "booksDataGridView";
+            this.booksDataGridView.ReadOnly = true;
+            this.booksDataGridView.Size = new System.Drawing.Size(460, 207);
+            this.booksDataGridView.TabIndex = 4;
+            // 
+            // Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.booksDataGridView);
+            this.Controls.Add(this.titleComboBox);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.authorComboBox);
+            this.Controls.Add(this.authorLabel);
+            this.Name = "Details";
+            this.Text = "Master/Detail";
+            this.Load += new System.EventHandler(this.Details_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.booksDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titleBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.authorBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label authorLabel;
+        private System.Windows.Forms.ComboBox authorComboBox;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.ComboBox titleComboBox;
+        private System.Windows.Forms.DataGridView booksDataGridView;
+        private System.Windows.Forms.BindingSource titleBindingSource;
+        private System.Windows.Forms.BindingSource authorBindingSource;
+    }
+}
+
---
+++ MasterDetail/MasterDetail/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterDetail
+{
+    public partial class Details : Form
+    {
+        public Details()
+        {
+            InitializeComponent();
+        }
+
+        private BooksDataContext database = new BooksDataContext();
+
+        private class AuthorBinding
+        {
+            public Author Author { get; set; }
+            public string Name { get; set; }
+        }
+
+        private void Details_Load(object sender, EventArgs e)
+        {
+            authorComboBox.DisplayMember = "Name";
+
+            authorComboBox.DataSource =
+                from author in database.Authors
+                orderby author.LastName, author.FirstName
+                select new AuthorBinding { Author = author, Name = author.FirstName + " " + author.LastName };
+
+            titleComboBox.DisplayMember = "Title1";
+
+            titleComboBox.DataSource =
+                from title in database.Titles
+                orderby title.Title1
+                select title;
+
+            booksDataGridView.DataSource = null;
+        }
+
+        private void authorComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Author currentAuthor = ((AuthorBinding)authorComboBox.SelectedItem).Author;
+
+            titleBindingSource.DataSource =
+                from book in currentAuthor.AuthorISBNs
+                select book.Title;
+
+            booksDataGridView.DataSource = titleBindingSource;
+        }
+
+        private void titleComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Title currentTitle = (Title)titleComboBox.SelectedItem;
+
+            authorBindingSource.DataSource =
+                from book in currentTitle.AuthorISBNs
+                select book.Author;
+
+            booksDataGridView.DataSource = authorBindingSource;
+        }
+    }
+}
